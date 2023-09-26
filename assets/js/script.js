2841ae8c56949418455f14bacd4356789a91487c
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-09-26 12:06:39 -0500

    Comment HTML, JS, and CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 // in the html.
 $(function () {
 
+  // Change class of time blocks relative to current time of day to set color
   var colorTimeBlocks = function() {
     var timeBlocks = $("div.time-block");
     var timeBlocksPast = timeBlocks.filter(function() {
@@ -20,6 +21,7 @@ $(function () {
     timeBlocksFuture.addClass("future").removeClass("present").removeClass("past");
   }
   
+  // Display confirmation message that appointment was saved in local storage
   var displayConfirmation = function() {
     var messageTimer = 4
     var messageInterval = setInterval(function() {
@@ -32,7 +34,8 @@ $(function () {
       }
     }, 100)
   }
-
+  
+  // Populate time blocks with saved data in local storage 
   $("div.time-block").each(function() {
       var hourKey = $(this).attr("id");
 
@@ -41,10 +44,12 @@ $(function () {
       }
     })
   
+  // Show current date
   $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
   colorTimeBlocks();
 
+  // Add event listener to save buttons, which save textarea values to local storage
   $("button.saveBtn").click(function() {
     var hourID = $(this).parent().attr("id");
     var textAreaValue = $(this).prev().val();
